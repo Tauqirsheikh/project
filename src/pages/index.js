190e5a8c6d6fce47fc80e954fd736e3570f7f3de
Touqeer Sheikh <mdsheikhtouqeer2000@gmail.com>
2025-01-23 15:00:28 +0530

    set padding

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Box sx={{ background: "#12372A" }}>
+      <Box sx={{ background: "#12372A", paddingTop: 8 }}>
         <Heading
           heading="Card payment"
           sx={{ color: "#FCFCFC", fontSize: "64px", fontWeight: 800 }}
